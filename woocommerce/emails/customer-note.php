f632c17c1b8c6c5761572453986ec9a604a4dc1f
niels <niels@luzus.fr>
2018-04-23 11:28:09 +0200

    modify customer-note.php (email template)

--- woocommerce/emails/customer-note.php
+++ woocommerce/emails/customer-note.php
@@ -611,12 +611,12 @@ if ( ! defined( 'ABSPATH' ) ) {
                      <tr>
                         <td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
 						<p>👋 Hello,</p>
-						<blockquote style="margin: 0;"><?php echo wpautop( wptexturize( $customer_note ) ) ?></blockquote><br><br>
+						<blockquote style="margin: 0; background-color:#eaeaea; min-height:20px"><?php echo wpautop( wptexturize( $customer_note ) ) ?></blockquote><br><br>
                         </td>
                     </tr>
                     <tr>
                     	<td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
-                            <?php _e( "For your reference, your order details are shown below.", 'woocommerce' ); ?><br>
+                            <?php _e( "For your reference, your order details are shown below.", 'woocommerce' ); ?><br><br>
 
                             <?php
                                 /**
@@ -657,28 +657,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     </tbody>
 </table>
 
-   
-   
-   
-   <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnDividerBlock" style="min-width:100%;">
-    <tbody class="mcnDividerBlockOuter">
-        <tr>
-            <td class="mcnDividerBlockInner" style="min-width:100%; padding:18px;">
-                <table class="mcnDividerContent" border="0" cellpadding="0" cellspacing="0" width="100%" style="min-width:100%;">
-                    <tbody><tr>
-                        <td>
-                            <span></span>
-                        </td>
-                    </tr>
-                </tbody></table>
-<!--            
-                <td class="mcnDividerBlockInner" style="padding: 18px;">
-                <hr class="mcnDividerContent" style="border-bottom-color:none; border-left-color:none; border-right-color:none; border-bottom-width:0; border-left-width:0; border-right-width:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0;" />
--->
-            </td>
-        </tr>
-    </tbody>
-</table><table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
     <tbody class="mcnTextBlockOuter">
         <tr>
             <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
