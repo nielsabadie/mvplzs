f00bf996e35829b286ca6aa88afe5b9f0837a0cd
niels <niels@luzus.fr>
2018-05-13 13:46:58 +0200

    unset more_seller_product

--- functions.php
+++ functions.php
@@ -646,7 +646,7 @@ add_filter('woocommerce_product_tabs', 'woo_remove_product_tabs', 98);
 function woo_remove_product_tabs($tabs)
 {
 
-    //unset($tabs['more_seller_product']);
+    unset($tabs['more_seller_product']);
     unset($tabs['specification']);
     unset($tabs['seller']);
     unset($tabs['reviews']);
